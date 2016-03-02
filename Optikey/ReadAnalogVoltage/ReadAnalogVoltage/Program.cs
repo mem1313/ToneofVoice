5655f928312d333846f448339bcec77c9923cbdb
RichardNiubility <fsky@vip.qq.com>
2016-03-02 17:47:49 -0500

    Added files via upload

---
+++ Optikey/ReadAnalogVoltage/ReadAnalogVoltage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ReadAnalogVoltage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = "COM4";
+            myport.Open();
+
+            while(true)
+            {
+                double voltage = 0;
+                string data = myport.ReadLine();
+                Double.TryParse(data, out voltage);
+                Console.WriteLine(voltage);
+            }
+        }
+    }
+}
